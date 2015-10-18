4d448f0b122922fe64e48f6e1eab6fbfec7cf6cf
Tassos Koutlas <tassos.koutlas@gmail.com>
2015-10-18 15:35:52 +0100

    Update App.php with code to call new Rovers.

--- src/MarsRover/App/App.php
+++ src/MarsRover/App/App.php
@@ -7,6 +7,7 @@
  */
 namespace MarsRover\App;
 
+use MarsRover\Rover\Rover;
 use League\CLImate\CLImate;
 use Exception;
 
@@ -23,6 +24,8 @@ class App
 
   private $cli;
   private $init = false;
+  private $initRover = false;
+  private $roverCommands = array();
 
   public function __construct()
   {
@@ -45,7 +48,25 @@ class App
       try {
 
         $this->validate($command);
+
+        if ($this->initRover) {
+          $rover = new Rover($this->roverCommands);
+          $rover->execute();
+          //$this->cli->green($rover->destination);
+
+          // Usually PHP garbage collection takes care of this but
+          // needs to be unset in large loops. See more at
+          // http://php.net/manual/en/function.unset.php#105980.
+          unset($rover);
+          
+          $this->roverCommands = array();
+          $this->initRover = false;
+
+          $this->cli->green('Rover executed.');
+          
+        }
         
+               
       } catch(Exception $e) {
 
         $this->cli->red($e->getMessage());
@@ -122,13 +143,36 @@ class App
   /**
    * Validates an Rover command.
    *
+   * This function splits rover commands into position and movement
+   * and populates the private argument $roverCommands. When that
+   * array has both values then a new Rover may be initialised.
+   *
    * @param string $command
    *   The input command from the user.
+   *
+   * @throws Exception
+   *   Throws an exception if the grid is not initialised first.
    */
   private function validateRoverCommand($command)
   {
-    
-    $this->cli->green('validateRoverCommand');
+
+    $size = strlen($command);
+
+    if ($this->init == false) {
+      throw new Exception('Need to initialise the grid first');
+    }
+
+    if ($size == 3) {
+      $this->roverCommands['position'] = $command;
+      $this->cli->green('Rover position set.');
+    } else {
+      $this->roverCommands['movement'] = $command;
+      $this->cli->green('Rover movement set.');
+    }
+
+    if (count($this->roverCommands) == 2) {
+      $this->initRover = true;
+    }
     
   }
 
