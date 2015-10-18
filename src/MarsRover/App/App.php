02d0746246c5cfc47312150fa033814a692864aa
Tassos Koutlas <tassos.koutlas@gmail.com>
2015-10-18 12:41:04 +0100

    Added phpdoc information on App class.

--- src/MarsRover/App/App.php
+++ src/MarsRover/App/App.php
@@ -1,10 +1,23 @@
 <?php
-
+/**
+ * This file contains the main app code for the Mars
+ * Rover problem.
+ *
+ * @author Tassos Koutlas <tassos.koutlas@gmail.com>
+ */
 namespace MarsRover\App;
 
 use League\CLImate\CLImate;
 use Exception;
 
+/**
+ * The main app class.
+ *
+ * This class is responsible for initialising and parsin initial
+ * commands and then assign the commands to appropriate methods
+ * for execution. It uses a Rover object to run commands on Mars
+ * Rovers.
+ */
 class App
 {
 
@@ -13,58 +26,90 @@ class App
 
   public function __construct()
   {
+    
     $this->cli = new CLImate();
+    
   }
-  
+
+  /**
+   * Starts the application.
+   */
   public function start()
   {
 
     $input = $this->cli->input('Please enter position/command: ');
     $input->defaultTo('You need to enter a command or a position.');
     
-    while($response = $input->prompt()) {
+    while($command = $input->prompt()) {
 
       try {
-        $this->validate($response);        
+
+        $this->validate($command);
+        
       } catch(Exception $e) {
+
         $this->cli->red($e->getMessage());
+        
       }
 
     }
     
   }
 
-  private function validate($response)
+  /**
+   * Validates a command.
+   *
+   * @param string $command
+   *   The input command from the user.
+   *
+   * @return void
+   *
+   * @throws Exception
+   *   Throws an exception if command is less than 2 characters.
+   */
+  private function validate($command)
   {
 
-    $init = $this->init;
-    $size = strlen($response);
+    $size = strlen($command);
 
-    if ($size == 2 && is_numeric($response) && $init == false) {
-      $this->validateGridCommand($response);
-      return;
-    }
-
-    if ($size == 2 && is_numeric($response) && $init == true) {
-      throw new Exception('Grid already initialised.');
+    if ($size == 2) {
+      $this->validateGridCommand($command);
       return;
     }
 
     if ($size >= 3) {
-      $this->validateRoverCommand($response);
+      $this->validateRoverCommand($command);
       return;
     }
 
     throw new Exception('Command is not correct.');
     return;
+    
   }
 
+  /**
+   * Validates an initialisation command and initialises the grid.
+   *
+   * @param string $command
+   *   The input command from the user.
+   *
+   * @return void
+   *
+   * @throws Exception
+   *   Throws exception if grid already initialised or command is not
+   *   two numbers.
+   */
   private function validateGridCommand($command)
   {
 
     $init = $this->init;
 
-    if (strlen($command) > 2 && !is_numeric($command)) {
+    if ($init) {
+      throw new Exception('Grid already initialised.');
+      return;
+    }
+    
+    if (!is_numeric($command)) {
       throw new Exception('Initialise command not correct');
     }
     else {
@@ -74,6 +119,12 @@ class App
 
   }
 
+  /**
+   * Validates an Rover command.
+   *
+   * @param string $command
+   *   The input command from the user.
+   */
   private function validateRoverCommand($command)
   {
     
@@ -81,6 +132,12 @@ class App
     
   }
 
+  /**
+   * Initialises the grid.
+   *
+   * @param string $command
+   *   The input command from the user.
+   */
   private function initialiseGrid($command)
   {
 
