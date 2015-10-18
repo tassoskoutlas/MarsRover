748fdf7e55b1073f9f10e263ef1222d19f0b8862
Tassos Koutlas <tassos.koutlas@gmail.com>
2015-10-18 20:30:14 +0100

    Updates on Rover class to turn and move the rover.

--- src/MarsRover/Rover/Rover.php
+++ src/MarsRover/Rover/Rover.php
@@ -6,6 +6,8 @@
  */
 namespace MarsRover\Rover;
 
+use Exception;
+
 /**
  * The Rover class.
  *
@@ -19,7 +21,9 @@ class Rover
   private $X;
   private $Y;
   private $O;
-
+  private $orientations = array('N', 'E', 'S', 'W');
+  private $movements = array('L', 'R', 'M');
+  
   public $destination;
   
 
@@ -41,15 +45,187 @@ class Rover
     $this->validate($this->position, 'position');
     $this->validate($this->movement, 'movement');
 
-    //$this->at($position);
-    //$this->to($movement);
+    $this->at($this->position);
+    $this->to($this->movement);
+    $this->destination = implode($this->getCurrentPosition());
+    
+  }
+
+  /**
+   * Puts a Rover at the specified position.
+   *
+   * @param string $position
+   *   A user specified string with positioning information.
+   */
+  private function at($position)
+  {
+    
+    $this->X = $position[0];
+    $this->Y = $position[1];
+    $this->O = $position[2];
+    
+  }
+
+  /**
+   * Moves a Rover accroding to the specified movement command.
+   *
+   * @param string $movement
+   *   A user specified string with movement information.
+   */
+  private function to($movement)
+  {
+
+    $commands = str_split($movement);
+
+    foreach ($commands as $command) {
+      $this->move($command);
+    }
+
+  }
+
+  /**
+   * Returns current position and orientation.
+   *
+   * @return array $current
+   *   An array with planar (x,y) values and orientation information.
+   */
+  private function getCurrentPosition()
+  {
+
+    $current = array();
+
+    $current['x'] = $this->X;
+    $current['y'] = $this->Y;
+    $current['o'] = $this->O;
+
+    return $current;
+  }
+
+  /**
+   * Execute a step of a move command.
+   *
+   * @param string $command.
+   *   A string of one character to execute a move command.
+   */
+  private function move($command)
+  {
+    
+    switch ($command) {
+
+    case 'L':
+      $this->turnLeft();
+      break;
+
+    case 'R':
+      $this->turnRight();
+      break;
+
+    case 'M':
+      $this->moveForward();
+      break;
+
+    }
+    
+  }
+
+  /**
+   * Returns the index of current orientation.
+   *
+   * Kappa is the position of the current orientation from the
+   * orientations array. It can be used to compute the left and
+   * right values when orientation change commants are issued.
+   *
+   * @param array $current
+   *   An array that contains current (x,y) and orientation values.
+   *
+   * @return int $kappa
+   *   An integer representing the position of the current orientation
+   *   in orientations array.
+   */ 
+  private function getKappa($current)
+  {
+    
+    $o = $current['o'];
+    $kappa = array_search($o, $this->orientations);
+    return $kappa;
+    
+  }
+
+  /**
+   * Turns the Rover left from its current orientation.
+   */
+  private function turnLeft()
+  {
+
+    $current = $this->getCurrentPosition();
+    $kappa = $this->getKappa($current);
+
+    if ($kappa + 1 == 4) {
+      $kappa = 0;
+    } else {
+      $kappa = $kappa + 1;
+    }
+    
+    $this->O = $this->orientations[$kappa];
+    
+  }
+
+  /**
+   * Turns the Rover right from its current orientation.
+   */
+  private function turnRight()
+  {
+
+    $current = $this->getCurrentPosition();
+    $kappa = $this->getKappa($current);
+
+    if ($kappa == 0) {
+      $kappa = 3;
+    } else {
+      $kappa = $kappa - 1;
+    }
+        
+    $this->O = $this->orientations[$kappa];
+    
+  }
+
+  /**
+   * Move the Rover forward depending on orientation.
+   */
+  private function moveForward()
+  {
+
+    $current = $this->getCurrentPosition();
+
+    switch ($current['o']) {
+
+    case 'N':
+      $current['y'] = $current['y'] + 1;
+      break;
+
+    case 'E':
+      $current['x'] = $current['x'] - 1;
+      break;
+
+    case 'S':
+      $current['y'] = $current['y'] - 1;
+      break;
+
+    case 'W':
+      $current['x'] = $current['x'] + 1;
+      break;
+
+    }
+
+    $position = implode($current);
+    $this->at($position);
     
   }
 
   /**
    * Validates Rover commands.
    *
-   * It validates a Rover command according to the $switch that is
+   * It validates a Rover command according to the switch that is
    * passed to it. The $p accepted is 'position' and 'movement'.
    *
    * @param string $command
@@ -65,17 +241,49 @@ class Rover
     switch ($op) {
 
     case 'position':
-      var_dump($command);
+      $this->validatePosition($command);
       break;
 
     case 'movement':
-      var_dump($command);
+      $this->validateMovement($command);
       break;
 
     }
     
   }
 
-  
-  
+  /**
+   * Validates a position command.
+   *
+   * @param string $command
+   *   A user specified command.
+   */
+  private function validatePosition($command)
+  {
+
+    $regex = '/[1-9]{2}[' . implode($this->orientations) . ']/';
+
+    if (!preg_match($regex, $command)) {
+      throw new Exception('Position information not correct. Enter again.');
+    }
+    
+  }
+
+  /**
+   * Validates a movement command.
+   *
+   * @param string $command
+   *   A user specified command.
+   */
+  private function validateMovement($command)
+  {
+    
+    $regex = '/[' . implode($this->movements) . ']+/';
+
+    if (!preg_match($regex, $command)) {
+      throw new Exception('Movement information not correct. Enter again.');
+    }
+    
+  }
+
 }
